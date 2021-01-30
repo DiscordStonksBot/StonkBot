6a2bce1df2e42b223806bbc75605fdb7343dd463
Lukshan <lukshan@hotmail.co.uk>
2021-01-30 04:04:41 +0000

    Update stockinfo.py

--- stockinfo.py
+++ stockinfo.py
@@ -7,26 +7,35 @@ import sys
 
 
 class StockInfo(commands.Cog):
+
+
     def __init__(self, client):
         self.client = client
+        self.LOCAL_TIMEZONE = datetime.datetime.now(datetime.timezone.utc).astimezone().tzinfo
+
+
+
+    def getSharePrice(self, ctx, share=None):
+        if (not share):
+            return(f"C'mon man. Gimme something to work with! (Error: share ticker symbol missing )")
+        else:
+            try:
+                v = sq.Stock(f'{share}')
+                t = datetime.datetime.now().strftime(f"%H:%M {self.LOCAL_TIMEZONE} (%m/%d/%Y)")
+                return(f"{share}: ${v.current_price} at {t}")
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
+                return(f"Bruh moment. I can't find a stock called {share}. Sort it out, dumbass.")
+
+
 
     @commands.command()
-    async def shareprice(self, ctx, share):
-        #await ctx.message.delete()
-        try:
-            v = sq.Stock(f'{share}')
-            t = datetime.datetime.now().strftime("%H:%M AKST (%m/%d/%Y)")
-            await ctx.channel.send(f"{share}: ${v.current_price} at {t}")
-        except:
-            print("Unexpected error:", sys.exc_info()[0])
-            await ctx.channel.send(f"Bruh moment. I can't find a stock called {share}. Sort it out, dumbass.")
-
+    async def shareprice(self, ctx, share=None):
+        await ctx.channel.send (self.getSharePrice(ctx, share))
+        
     
     @commands.command()
-    async def sp(self, ctx, share):
-        await ctx.message.delete()
-        v = sq.Stock(f'{share}')
-        t = datetime.datetime.now().strftime("%H:%M AKST (%m/%d/%Y)")
-        await ctx.channel.send(f"{share}: ${v.current_price} at {t}")
+    async def sp(self, ctx, share=None):
+        await ctx.channel.send (self.getSharePrice(ctx, share))
     
 def setup(client): client.add_cog(StockInfo(client))
